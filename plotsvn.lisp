4be700a84b1fe67e25671feccb3f2d6b69a7f21a
Florian MARGAINE <florian@margaine.com>
2014-08-01 09:29:06 +0200

    Uses better "quit" function.

--- plotsvn.lisp
+++ plotsvn.lisp
@@ -4,26 +4,31 @@
 
 ;;; "plotsvn" goes here. Hacks and glory await!
 
+;; A simple macro to quickly exit the app.
+;; A macro instead of a function to let functions
+;; handle format's arguments as they wish.
+(defmacro quit (&body body)
+  `(progn
+     (format t ,@body)
+     (sb-thread:abort-thread :allow-exit t)))
+
 ;; Function ran from command-line
 (defun main (argv)
   (let ((filename (second argv)))
     (unless filename
-      (format t "XML file required.~%")
-      (quit))
+      (quit "XML file required.~%"))
     (let* ((xml (read-xml filename))
            (logentries (cdr xml))
            (plot-type (third argv)))
       (plot (cond
               ((string= "commits-by-date" plot-type) (commits-by-date logentries))
-              (t (format t "No plot specified.~%")))))))
+              (t (quit "No plot specified.~%")))))))
 
 ;; Reads the XML file and returns it as a string.
 (defun read-xml (filename)
   (handler-case (s-xml:parse-xml-string (file-as-string filename))
-    (s-xml:xml-parser-error () (progn
-                                 (format t "Malformed XML.~%")
-                                 (quit)))))
-  
+    (s-xml:xml-parser-error () (quit "Malformed XML.~%"))))
+
 ;; Utility functions
 (defun file-as-string (file)
   (format nil
@@ -32,6 +37,3 @@
             (loop for line = (read-line s nil 'eof)
                  until (eq line 'eof)
                  collect line))))
-
-(defun quit ()
-  (sb-thread:abort-thread :allow-exit t))
