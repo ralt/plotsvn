d5726835ed2d47cbe2bce92c4d9b76e7b74e6d6e
Florian Margaine <florian@margaine.com>
2014-08-01 19:43:33 +0200

    Outputs png instead of ps and take control back

--- gnuplot.lisp
+++ gnuplot.lisp
@@ -3,6 +3,14 @@
 (defun plot (fn logentries)
   "Uses GNUPlot to plot the points."
   (cgn:start-gnuplot)
+  ; Don't show the filename
+  (cgn:format-gnuplot "set nokey")
+  ; Call the plotting function with a single argument
   (apply fn (list logentries))
-  (cgn:postscript-copy "your-plot.ps")
+  ; Join the lines between plotted points
+  (cgn:format-gnuplot "plot '.cgn.dat' with linesp")
+  ; Save the file to an image
+  (cgn:format-gnuplot "set term png")
+  (cgn:format-gnuplot "set output 'output.png'")
+  (cgn:format-gnuplot "replot")
   (cgn:close-gnuplot))
--- plot.lisp
+++ plot.lisp
@@ -2,9 +2,8 @@
 
 (defun commits-by-date (logentries)
   "Plots authors with commits by date."
+  (declare (ignore logentries))
   (cgn:set-title "Commits by date")
   (cgn:set-range 'cgn:x 0 5)
   (cgn:set-range 'cgn:y 0 10)
-  (cgn:plot-points '(1 2) '(3 4))
-  (cgn:format-gnuplot "set nokey")
-  (cgn:format-gnuplot "plot '.cgn.dat' with linesp"))
+  (cgn:plot-points '(1 2) '(3 4)))
